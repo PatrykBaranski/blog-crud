82691a1b1f3b416eed98629c663163879a0ca797
Patryk Barański <pbaranski740@gmail.com>
2024-01-04 22:57:45 +0100

    Init commit

---
+++ src/redux/postsReducer.js
@@ -0,0 +1,10 @@
+const createActionName = (actionName) => `app/posts${actionName}`;
+
+const postsReducer = (statePart = [], action) => {
+  switch (action.type) {
+    default:
+      return statePart;
+  }
+};
+
+export default postsReducer;
